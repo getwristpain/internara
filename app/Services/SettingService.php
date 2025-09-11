142bdba1ce659006a94c6ed4818a568237139d6c
Reas Vyn <reasnov.app@gmail.com>
2025-09-11 15:18:45 +0700

    add error handler in SettingService

--- app/Services/SettingService.php
+++ app/Services/SettingService.php
@@ -8,6 +8,7 @@ use App\Helpers\LogicResponse;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Schema;
 
 class SettingService
 {
@@ -25,100 +26,100 @@ class SettingService
     public function get(string|array $keys, mixed $default = null): mixed
     {
         try {
-            DB::connection()->getPdo();
+            if (is_array($keys)) {
+                $models = Setting::query()->whereIn('key', $keys)
+                    ->pluck('value', 'key');
+
+                return collect($keys)
+                    ->mapWithKeys(function ($key) use ($models, $default) {
+                        if (!$this->isInstalled() && str_starts_with($key, 'brand_')) {
+                            return match ($key) {
+                                'brand_name' => config('app.name'),
+                                'brand_logo' => config('app.logo'),
+                                default => null,
+                            };
+                        }
+
+                        if (is_array($default) && empty($models[$key]) && isset($default[$key])) {
+                            return [$key => $default['key']];
+                        }
+
+                        return [$key => $models[$key]];
+                    })
+                    ->toArray() ?? [];
+            }
+
+            if ($keys === 'brand_name' && !$this->isInstalled()) {
+                return config('app.name');
+            }
+
+            if ($keys === 'brand_logo' && !$this->isInstalled()) {
+                return config('app.logo');
+            }
+
+            return Setting::where('key', $keys)->first()?->value ?? $default;
         } catch (\Throwable $th) {
+            LogicResponse::make()->debug($th);
+        } finally {
             return null;
         }
-
-        if (is_array($keys)) {
-            $models = Setting::query()->whereIn('key', $keys)
-                ->pluck('value', 'key');
-
-            return collect($keys)
-                ->mapWithKeys(function ($key) use ($models, $default) {
-                    if (!$this->isInstalled() && str_starts_with($key, 'brand_')) {
-                        return match ($key) {
-                            'brand_name' => config('app.name'),
-                            'brand_logo' => config('app.logo'),
-                            default => null,
-                        };
-                    }
-
-                    if (is_array($default) && empty($models[$key]) && isset($default[$key])) {
-                        return [$key => $default['key']];
-                    }
-
-                    return [$key => $models[$key]];
-                })
-                ->toArray() ?? [];
-        }
-
-        if ($keys === 'brand_name' && !$this->isInstalled()) {
-            return config('app.name');
-        }
-
-        if ($keys === 'brand_logo' && !$this->isInstalled()) {
-            return config('app.logo');
-        }
-
-        return Setting::where('key', $keys)->first()?->value ?? $default;
     }
 
     public function cached(string|array $keys, mixed $default = null, int $ttl = 3600): mixed
     {
         try {
-            DB::connection()->getPdo();
+            if (is_array($keys)) {
+                return collect($keys)->mapWithKeys(fn ($k) => [
+                    $k => Cache::remember(
+                        "settings.{$k}",
+                        $ttl,
+                        fn () => $this->get($k)
+                    )
+                ])->all() ?? $default;
+            }
+
+            return Cache::remember(
+                "settings.{$keys}",
+                $ttl,
+                fn () => $this->get($keys, $default)
+            );
         } catch (\Throwable $th) {
+            LogicResponse::make()->debug($th);
+        } finally {
             return null;
         }
-
-        if (is_array($keys)) {
-            return collect($keys)->mapWithKeys(fn ($k) => [
-                $k => Cache::remember(
-                    "settings.{$k}",
-                    $ttl,
-                    fn () => $this->get($k)
-                )
-            ])->all() ?? $default;
-        }
-
-        return Cache::remember(
-            "settings.{$keys}",
-            $ttl,
-            fn () => $this->get($keys, $default)
-        );
     }
 
     public function set(array|string $keys, mixed $value = null): Setting|int|null
     {
         try {
-            DB::connection()->getPdo();
+            if (is_array($keys)) {
+                if (! Arr::isAssoc($keys)) {
+                    throw new \InvalidArgumentException('The $keys parameter must be an associative array.');
+                }
+
+                $i = 0;
+
+                foreach ($keys as $key => $val) {
+                    $this->set($key, $val);
+                    $i++;
+                }
+
+                return $i;
+            }
+
+            $setting = Setting::updateOrCreate(
+                ['key' => $keys],
+                ['value' => $value]
+            );
+
+            Cache::put("settings.{$keys}", $value, now()->addHour());
+            return $setting;
         } catch (\Throwable $th) {
+            LogicResponse::make()->debug($th);
+        } finally {
             return null;
         }
-
-        if (is_array($keys)) {
-            if (! Arr::isAssoc($keys)) {
-                throw new \InvalidArgumentException('The $keys parameter must be an associative array.');
-            }
-
-            $i = 0;
-
-            foreach ($keys as $key => $val) {
-                $this->set($key, $val);
-                $i++;
-            }
-
-            return $i;
-        }
-
-        $setting = Setting::updateOrCreate(
-            ['key' => $keys],
-            ['value' => $value]
-        );
-
-        Cache::put("settings.{$keys}", $value, now()->addHour());
-        return $setting;
     }
 
     public function isInstalled(): bool
